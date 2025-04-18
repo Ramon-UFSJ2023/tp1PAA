d12eaed4fb4bf5354e9cc04740448da1cb1088b6
Ramon Caique De Paula <132967661+Ramon-UFSJ2023@users.noreply.github.com>
2025-04-18 11:32:50 -0300

    que deus me leve n aguento mais

--- forca_bruta/main.c
+++ forca_bruta/main.c
@@ -17,32 +17,33 @@ int main(){
 
     while(1){
         fscanf(arq, "%d %d", &NM[0], &NM[1]); // Leio o tamanho de linha e colunas do txt
-        fgetc(arq); // captura o \r
+        //fgetc(arq); // captura o \r que so tem no win
         fgetc(arq); // captura o \n
         int contPlayer1 = contPlayer2 = contLine = contColun = contEnter= 0;
         if(verify(NM) == 0) break; // função que verifica as condições
         int **matriz;
         constMatriz(&matriz, NM); // função que aloca a matriz multiplicando N*M
 
-        while((charOfSecondLine = fgetc(arq)) != '\n'){
-            if (charOfSecondLine == '\r' || charOfSecondLine == EOF || contColun> NM[1] || contLine > NM[0]) break; // leio proxima linha da entrada
+        while((charOfSecondLine == fgetc(arq)) != '\r'){
+            //printf("Ola\n");
             if(contColun > NM[1]-1){
                 contLine++;
                 if(contLine % 2 == 0){
                     contColun = 0;
                 }else{contColun = 1;} // garanto que as peças serão posicionadas em diagonal nas linhas
             }
+            if(contLine> NM[0]) break;
             contEnter++; // numero total da entrada de descrições
             if(verifyNumPecas(&contPlayer1, &contPlayer2, NM, charOfSecondLine) == 0) exit(1);
             matriz[contLine][contColun]= charOfSecondLine -'0'; // necessario para conversão do caractere para Inteiro
             contColun+= 2;
         }
-
-        if(contEnter != (int)ceil(NM[0]*NM[1]/2)){ // problema aq
+        printf("Ola\n");
+        
+        if(contEnter != (NM[0]*NM[1]/2)+1){ // problema aq
             liberarMatriz(matriz, NM);
             break;
         }
-
         for(int i=0; i< NM[0];i++){
             for (int j=0; j< NM[1];j++){ 
                 if(matriz[i][j] == 1){ // percorre a matriz até achar uma das minhas peças
@@ -53,6 +54,7 @@ int main(){
 
             }
         }
+        
         liberarMatriz(matriz, NM);
         printf("%d \n", MaxPecas);
 
