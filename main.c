62a4343c22b1cf47abd505ca7364512b3af29a11
Ramon Caique De Paula <132967661+Ramon-UFSJ2023@users.noreply.github.com>
2025-04-25 10:19:10 -0300

    algumas correções

--- main.c
+++ main.c
@@ -4,14 +4,17 @@
 #include "board.h"
 #include "capture.h"
 #include <sys/time.h>
+#include <sys/resource.h>
 #include <unistd.h>
 #include <math.h>
 
 int main(){
     int NM[2];
     FILE *arq = fopen("ent.txt", "r");
+    FILE *arqOut = fopen("saida.txt", "w");
 
     struct timeval time_start1, time_end1, time_start2, time_end2;
+    struct rusage ru_start, ru_end;
 
     if(arq == NULL){
         printf("Error, Arquivo Inexistente.\n");
@@ -34,31 +37,36 @@ int main(){
         switch (choose){
 
         case 1:
-            printf("Tempo com algoritmo Euristico: \n");
+            
             if (gettimeofday(&time_start1, NULL) != 0) {
                 perror("gettimeofday");
                 exit(EXIT_FAILURE);
             }
 
             int max_captures = findMaxCaptures(t, 1); //Algoritmo Euristico
-            printf("%d\n", max_captures);
+            fprintf(arqOut, "%d\n", max_captures);
 
             if (gettimeofday(&time_end1, NULL) != 0) {
                 perror("gettimeofday");
+                exit(EXIT_FAILURE);
+            }
+
+            if (getrusage(RUSAGE_SELF, &usage) != 0) {
+                perror("getrusage");
                 exit(EXIT_FAILURE);
             }
             long sec1=time_end1.tv_sec-time_start1.tv_sec;
             break;
 
         case 2:
-            printf("Tempo com algoritmo Força Bruta: \n");
+
             if (gettimeofday(&time_start2, NULL) != 0) {
                 perror("gettimeofday");
                 exit(EXIT_FAILURE);
             }
 
             int maxPecas = algForBru(NM[0], NM[1], matriz); //algoritmo força bruta
-            printf("%d \n", maxPecas);
+            fprintf(arqOut, "%d\n", max_captures);
 
             if (gettimeofday(&time_end2, NULL) != 0) {
                 perror("gettimeofday");
@@ -70,8 +78,15 @@ int main(){
         default:
             break;
         }
+        printf("Tempo com algoritmo Euristico: \n");
+        printf("")
+
+        printf("Tempo com algoritmo Força Bruta: \n");
+
+
 
     }
+
     fclose(arq);
     return 0;
 }
---
+++ readme.txt
@@ -0,0 +1,2 @@
+Para execução do programa só é necessario dar make e após isso executar no terminal o seguinte comando "./tp1". 
+A saida e entrada são lidas e escritas automaticamente nos txt já criados breviamente.
